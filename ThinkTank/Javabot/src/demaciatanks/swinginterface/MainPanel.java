f4f3aa4242ab30f6909fda382e50f7059d40e447
Pacôme <Pacme@192.168.1.35>
2015-09-23 19:32:18 +0200

    Ajout d'un package Demaciatanks. Mettez vos futurs sous package dans ce dossier.

---
+++ ThinkTank/Javabot/src/demaciatanks/swinginterface/MainPanel.java
@@ -0,0 +1,12 @@
+package demaciatanks.swinginterface;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel {
+	
+	private static final long serialVersionUID = 5131738883703122146L;
+
+	public MainPanel() {
+	}
+
+}
---
+++ ThinkTank/Javabot/src/demaciatanks/swinginterface/Window.java
@@ -0,0 +1,13 @@
+package demaciatanks.swinginterface;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+	private static final long serialVersionUID = -8179346049381197847L;
+
+	public Window() throws HeadlessException {
+	}
+}
